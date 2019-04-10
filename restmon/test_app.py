957b8e8e20366df05feda4c5ea3a661e2e49cd75
vladtheloka <lokatheloka@gmail.com>
2019-04-10 13:58:14 +0300

    Add more unit tests

--- restmon/test_app.py
+++ restmon/test_app.py
@@ -35,3 +35,44 @@ class FlaskAppTests(unittest.TestCase):
 
         # assert the response data
         self.assertEqual(result.data, '{"Hello": "World"}\n')
+
+    def test_cpu_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/cpu')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_cpu_percent_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/cpupercent')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_memory_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/memory')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_storage_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/storage')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_platform_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/platform')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
